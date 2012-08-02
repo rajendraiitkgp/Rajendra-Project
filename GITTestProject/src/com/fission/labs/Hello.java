776b953ffb189dae1fed1cac503e919226d23d3a
Rajebdra Prasad M <Rajebdra Prasad M@FCL9996-PC>
2012-08-02 12:34:41 +0530

    Added one print statement

---
+++ GITTestProject/src/com/fission/labs/Hello.java
@@ -0,0 +1,16 @@
+package com.fission.labs;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is fission labs.");
+		System.out.println("This is Rajendra Prasad Maurya");
+		System.out.println("THis is Rajendra.....");
+
+	}
+
+}
